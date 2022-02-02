3b6427e56d3b938c3ea0370b8b04567a1b058a83
pixls <tha.pixls@gmail.com>
2022-02-02 18:13:39 -0500

    board rendering works

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask
+from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -9,3 +9,11 @@ app.config['SECRET_KEY'] = 'my-secret-key'
 toolbar = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+
+@app.route('/')
+def boggle():
+
+    game_board = boggle_game.make_board()
+
+    return render_template('board.html', board=game_board)
