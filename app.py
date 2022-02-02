9446a1fe78932ad99bc3871f8787d3c09cf2f36a
pixls <tha.pixls@gmail.com>
2022-02-02 12:30:11 -0500

    set up app file

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from boggle import Boggle
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'my-secret-key'
+
+toolbar = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
