32ebb8e44bfbb04762574129daff0236108abd42
pixls <tha.pixls@gmail.com>
2022-02-03 11:04:54 -0500

    added styling

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 toolbar = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
-
+game_board = boggle_game.make_board()
 
 @app.route('/')
 def board_render():
 
-    game_board = boggle_game.make_board()
+    global game_board
 
     return render_template('board.html', board=game_board)
